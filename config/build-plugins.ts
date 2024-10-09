0f9373bea647cdba9dd1eb43e9ec587e0f77a864
comradeskolopendra <comradeskolopendra@gmail.com>
2024-10-09 20:01:20 +0700

    Настроить пути и copy-plugin webpack

--- config/build-plugins.ts
+++ config/build-plugins.ts
@@ -3,6 +3,8 @@ import { BuildOptions } from "config-types";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import CopyPlugin from "copy-webpack-plugin";
+import path from "path";
 
 export function buildPlugins({ mode, analyzer, paths, port }: BuildOptions): Configuration["plugins"] {
     const isDev = mode === "development";
@@ -21,6 +23,11 @@ export function buildPlugins({ mode, analyzer, paths, port }: BuildOptions): Con
             new MiniCssExtractPlugin({
                 filename: "css/[name].[contenthash:8].css",
                 chunkFilename: "css/[name].[contenthash:8].css"
+            }),
+            new CopyPlugin({
+                patterns: [
+                    { from: path.resolve(paths.public, "assets/"), to: "assets/" }
+                ]
             })
         )
     }
